89a093b2a4b983cfdf5e8d8dc1134930ac4ce016
I3anaan <I3anaan@live.nl>
2014-08-07 13:14:55 +0200

    tower buttons

--- Assets/Scripts/Controllers/GameGUI.cs
+++ Assets/Scripts/Controllers/GameGUI.cs
@@ -8,12 +8,21 @@ public class GameGUI : MonoBehaviour
 	public float guiAspectRatio = 0.5f;
 	private float guiWidth;
 	private float guiPercentage;
+	private GameController gc;
+
+	public BasicTower[] towers;
+
+
+	public void Start ()
+	{
+		gc = GameController.getInstance ();
+	}
 
 
 	//Temporary rescale every frame.
 	public void FixedUpdate ()
 	{
-		print ("W" + Screen.width + "  H" + Screen.height);
+		//print ("W" + Screen.width + "  H" + Screen.height);
 		guiWidth = (float)Screen.height * (float)guiAspectRatio;
 		guiPercentage = guiWidth / Screen.width;
 		camera.rect = new Rect (guiPercentage, 0, 1 - guiPercentage, 1);
@@ -21,8 +30,14 @@ public class GameGUI : MonoBehaviour
 
 	public void OnGUI ()
 	{
-		GUI.Box (new Rect (0, 0, guiWidth, Screen.height), "Test GUI");
+		//GUI.Box (new Rect (0, 0, guiWidth, Screen.height), "NIET TEST GUI");
 
+		for (int i=0; i<towers.Length; i++) {
+			BasicTower tower = towers [i];
+			if (GUI.Button (new Rect (0, i * 50, guiWidth, 50), tower.name)) {
+				gc.towerSelected = tower;
+			}
+		}
 	}
 
 
